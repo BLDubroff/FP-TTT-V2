48fa5ae0a0129452c0c5496f7693d01e7ca591b8
Ben Dubroff <BLDubroff@gmail.com>
2023-02-01 18:11:09 -0500

    js endGame and draw functions

--- 3x3.js
+++ 3x3.js
@@ -46,3 +46,18 @@ function handleClick(e) {
       setBoardHoverClass()
     }
   }
+
+  function endGame(draw) {
+    if (draw) {
+      winningMessageTextElement.innerText = 'Draw!'
+    } else {
+      winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+  }
+  
+  function isDraw() {
+    return [...cellElements].every(cell => {
+      return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    })
+  }
